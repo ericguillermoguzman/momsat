f34203fd43d2658ab0885b3ce5ea366eba78abd5
Eric Guzman <109895013+ericguillermoguzman@users.noreply.github.com>
2023-02-09 21:07:28 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -15,4 +15,24 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+const images = [
+  "assets/003.jpg",
+  "assets/005.jpg",
+  "assets/002.jpg",
+  "assets/004.jpg",
+  // Add more images as needed
+];
+
+let currentImageIndex = 0;
+
+
+
+function changeBackground() {
+  const container = document.getElementById("bg");
+  container.style.backgroundImage = `url(${images[currentImageIndex]})`;
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+}
+
+setInterval(changeBackground, 10000); // Change the background every 5 seconds
+
 }
